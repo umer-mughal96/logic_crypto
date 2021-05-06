f88c163dfc1c9af8e080615f1b53e88d27d5c402
unknown <itsmeumer96@gmail.com>
2021-05-06 14:17:16 -0700

    asdasd

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Routes from "./utils/routes";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  axios.defaults.baseURL = "http://localhost:3001/logiccrypto/api/v1";
+  // axios.defaults.baseURL = "http://localhost:3001/logiccrypto/api/v1";
+  axios.defaults.baseURL = "https://logiccryptobackened.herokuapp.com/logiccrypto/api/v1";
   return (
     <Fragment>
       <Router>
