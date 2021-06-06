4e21d0c4273f076e9cff5d92900a3b203b2f51a4
unknown <itsmeumer96@gmail.com>
2021-06-06 17:56:15 +0500

    Deployed to Production

--- src/App.js
+++ src/App.js
@@ -2,34 +2,24 @@ import { Fragment, useEffect } from "react";
 import axios from "axios";
 import Routes from "./utils/routes";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useSelector , useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setAuthToken } from "./utils/setAuthToken";
 import { io } from "socket.io-client";
 
 const App = () => {
-  // axios.defaults.baseURL = "http://localhost:3001/logiccrypto/api/v1";
-
   const { user } = useSelector((x) => x.Auth);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (user) {
-      const socket = io("http://localhost:3001");
+      const socket = io(process.env.REACT_APP_BACKEND_SERVER);
       socket.on("userConnected", (dataFromServer) => {
         socket.emit("userInfo", { data: user });
-        socket.on('activeUsers' , connectedUsers => {
-        console.log("🚀 ~ file: App.js ~ line 21 ~ socket.on ~ connectedUsers", connectedUsers)
-        // dispatch()
-          
-        })
       });
     }
   }, [user]);
 
-
-
-  axios.defaults.baseURL =
-    "https://logiccryptobackened.herokuapp.com/logiccrypto/api/v1";
+  axios.defaults.baseURL = process.env.REACT_APP_BACKEND_SERVER;
   const { token } = useSelector((state) => state.Auth);
   setAuthToken(token);
   return (
