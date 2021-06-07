e22a64e3c4bfe5904905a068c308f24e12cff53f
unknown <itsmeumer96@gmail.com>
2021-06-07 13:39:11 +0500

    New Merging

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -17,9 +17,9 @@ import AuthenticatedRoute from "../routes/AuthenticatedRoute";
 import ForgotPassword from "../screens/ForgotPassword";
 import ResetPassword from "../screens/ResetPassword";
 import NewRule from "../screens/NewRule";
-<<<<<<< HEAD
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import Payment from "../screens/Payment";
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
@@ -54,6 +54,7 @@ const Routes = withRouter(({ location }) => {
           <Route path="/forgotpassword" component={ForgotPassword} />
           <Route path="/resetpassword" component={ResetPassword} />
           <Route path="/invest" component={Invest} />
+          <Route path="/pay" component={Payment} />
         </Switch>
         {location.pathname == "/setting-dsh" ||
         location.pathname == "/rule-dsh" ||
@@ -63,46 +64,6 @@ const Routes = withRouter(({ location }) => {
         )}
       </Fragment>
     </Elements>
-=======
-import Payment from "../screens/Payment";
-
-const Routes = withRouter(({ location }) => {
-  return (
-    <Fragment>
-      {location.pathname == "/setting-dsh" ||
-      location.pathname == "/rule-dsh" ||
-      location.pathname == "/rule" ||
-      location.pathname == "/exchange-dsh" ? null : (
-        <Header />
-      )}
-      <Switch>
-        <Route exact path="/" component={AutomatedTrading} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/signin" component={Signin} />
-        <AuthenticatedRoute path="/setting-dsh" component={SettingDashboard} />
-        <AuthenticatedRoute path="/rule-dsh" component={RulesDashboard} />
-        <AuthenticatedRoute path="/rule" component={NewRule} />
-        <AuthenticatedRoute
-          path="/exchange-dsh"
-          component={ExchangeDashboard}
-        />
-        <Route path="/pricing" component={Pricing} />
-        <Route path="/knowledge" component={Knowledge} />
-        <Route path="/enth" component={Enthuasists} />
-        <Route path="/categories" component={Categories} />
-        <Route path="/forgotpassword" component={ForgotPassword} />
-        <Route path="/resetpassword" component={ResetPassword} />
-        <Route path="/invest" component={Invest} />
-        <Route path="/pay" component={Payment} />
-      </Switch>
-      {location.pathname == "/setting-dsh" ||
-      location.pathname == "/rule-dsh" ||
-      location.pathname == "/rule" ||
-      location.pathname == "/exchange-dsh" ? null : (
-        <Footer />
-      )}
-    </Fragment>
->>>>>>> 165f4bcd599d8149cfe9a333622b36a338e3b4ac
   );
 });
 
